f7c84aad5091615793549e9282d72625f5e14837
Dustin Davis <davisdustin328@gmail.com>
2024-05-07 13:58:16 -0500

    added right packages

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,25 +1,38 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[hash][ext][query]'
-        }
-      }
-    ]
-  },
-  mode: 'production'
+ entry: './js/dashboard_main.js',
+ output: {
+   filename: 'bundle.js',
+   path: path.resolve(__dirname, 'public')
+ },
+ module: {
+   rules: [
+     {
+       test: /\.css$/,
+       use: ['style-loader', 'css-loader']
+     },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       use: [
+         {
+           loader: 'file-loader',
+           options: {
+             name: '[path][name].[ext]',
+           }
+         },
+         {
+           loader: 'image-webpack-loader',
+           options: {
+             mozjpeg: {
+               progressive: true,
+               quality: 65
+             },
+           }
+         }
+       ]
+     }
+   ]
+ },
+ mode: 'production'
 };
