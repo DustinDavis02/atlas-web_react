4254f20852bd0786db38fd155f27bdd5860322cd
Dustin Davis <dustindavis328@gmail.com>
2024-06-06 20:10:52 -0500

    update for 22

--- react_redux_connectors_and_providers/task_6/dashboard/src/selectors/courseSelector.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { List } from 'immutable';
-
-const getCourses = (state) => state.courses;
-
-export const getAllCourses = createSelector(
-  [getCourses],
-  (courses) => courses.valueSeq().toList()
-);
